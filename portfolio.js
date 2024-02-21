c79aa0a5be4c5fad774a72bbbfa788ab6a43856d
KesaniRavichandra <138872407+KesaniRavichandra@users.noreply.github.com>
2024-02-21 16:41:15 +0530

    Add files via upload
    ok

---
+++ portfolio.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll("nav a");
+  
+    for (const link of links) {
+      link.addEventListener("click", smoothScroll);
+    }
+  
+    function smoothScroll(event) {
+      event.preventDefault();
+      const targetId = event.currentTarget.getAttribute("href");
+      const targetPosition = document.querySelector(targetId).offsetTop - 50;
+      window.scroll({
+        top: targetPosition,
+        behavior: "smooth"
+      });
+    }
+  });
